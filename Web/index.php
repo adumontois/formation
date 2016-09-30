c880ace90f3b49debd0268dbd50b33d250898eb2
adumontois <a.dumontois@dreamcentury.com>
2016-09-30 12:54:39 +0200

    Initialisation du projet site web

---
+++ Web/index.php
@@ -0,0 +1,18 @@
+<?php
+	echo 'Hello World 2 !';
+
+	/*
+	// Exemple serialize
+	$srzed = serialize(array('nom' => 'Candeloro', 'prenom' => 'Philippe'));
+	echo $srzed;
+	echo '</br>';
+	$redirect = "Location:autrepage.php?data=".urlencode($srzed);
+	echo $redirect;
+	echo '</br>';
+	$data = substr($redirect, strlen("Location:autrepage.php?data="));
+	echo $data;
+	echo '</br>';
+	print_r (unserialize(urldecode($data)));
+	*/
+	
+?>
