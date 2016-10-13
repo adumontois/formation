6e77cd5a3728b8827c000d0d8d1f91c57e4d78d6
adumontois <a.dumontois@dreamcentury.com>
2016-10-13 15:32:19 +0200

    Added asimple button to insert an arbitrary comment

--- App/Frontend/Modules/News/Views/buildNews.json.php
+++ App/Frontend/Modules/News/Views/buildNews.json.php
@@ -26,12 +26,13 @@
 	</p>
 <?php endif; ?>
 
-<p>
+<p class="no-comment">
 	<a href="<?= $link_a[ 'putInsertComment' ] ?>">Ajouter un commentaire</a>
+	<button class="insert-comment">Insérer</button>
 </p>
 
 <?php if ( empty( $Comment_list_a ) ): ?>
-	<p>
+	<p id="no-comment">
 		Aucun commentaire n'a encore été posté. Soyez le premier à en laisser un !
 	</p>
 <?php endif; ?>
@@ -53,36 +54,52 @@
 	</fieldset>
 <?php endforeach; ?>
 
-<p>
+<p class="no-comment">
 	<a href="<?= $link_a[ 'putInsertComment' ] ?>">Ajouter un commentaire</a>
+	<button class="insert-comment">Insérer</button>
+</p>
 
-	
-	
-	
-	
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js"></script>
 <script type="text/javascript">
-	// Test
-	var comment = {
-		author  : 'Mon_auteur',
-		content : 'Ceci est un contenu.',
-		date    : '13/10/2016 à 14h24'
-	};
-	
-	// Transformer la variable en string JSON
-	var string = JSON.stringify( comment );
-	var html   = jQuery.parseJSON( string );
-	
-	// On a construit un objet JS à partir du JSON. Maintenant on veut afficher le nouveau commentaire
-	
-	node = document.createElement('fieldset');
-	node2 = document.createElement('legend');
-	node.appendChild(node2);
-	node2.innerHTML = 'Posté par <strong>'+html['author']+'</strong> le '+html[ 'date' ];
-	node3 = document.createElement('p');
-	node.appendChild(node3);
-	node3.textContent = html['content'];
-	//console.log(document.getElementsByTagName('fieldset'));
-	node = document.getElementById('main').insertBefore(node, document.getElementsByTagName('fieldset')[0]);
+	$( function() {
+		$( ".insert-comment" ).click( function() {
+			// Test
+			var comment = {
+				author  : 'Mon_auteur',
+				content : 'Ceci est un contenu.',
+				date    : '13/10/2016 à 14h24'
+			};
+			
+			// Transformer la variable en string JSON
+			var string = JSON.stringify( comment );
+			var html   = jQuery.parseJSON( string );
+			
+			// On a construit un objet JS à partir du JSON. Maintenant on veut afficher le nouveau commentaire
+			
+			var new_comment = $( "<fieldset></fieldset>" )
+					.append( $( "<legend></legend>" ).append( "Posté par ", $( "<strong></strong>" ).text( html[ 'author' ] ), ' le ', html[ 'date' ] ) );
+			new_comment.prepend( $( "<p></p>" ).text( comment[ 'content' ] ) );
+			
+			/*
+			 node2 = document.createElement( 'legend' );
+			 node.appendChild( node2 );
+			 node2.innerHTML = 'Posté par <strong>' + html[ 'author' ] + '</strong> le ' + html[ 'date' ];
+			 node3           = document.createElement( 'p' );
+			 node.appendChild( node3 );
+			 node3.textContent = html[ 'content' ];
+			 */
+			
+			// Sélectionner l'endroit d'insertion
+			var insert_location = $( "#main fieldset" )[ 0 ];
+			if ( typeof insert_location === 'undefined' ) {
+				insert_location = $( "#main .no-comment" )[ 1 ];
+				// On supprime le message disant qu'il n'y a pas de commentaire
+				$( "#no-comment" ).remove();
+			}
+			
+			// Insérer le noeud
+			$( new_comment ).insertBefore( insert_location );
+		} )
+	} )
 </script>
