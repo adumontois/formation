8d8260b38999f2fa75a9374e0cb04ddd7c5758e2
adumontois <a.dumontois@dreamcentury.com>
2016-10-13 14:49:02 +0200

    Inserted an arbitrary content into buildNews with JS

--- App/Frontend/Modules/News/Views/buildNews.json.php
+++ App/Frontend/Modules/News/Views/buildNews.json.php
@@ -9,57 +9,80 @@
 /**
  * @var $News              \Entity\News News à afficher
  * @var $Comment_list_a    \Entity\Comment[] Liste des commentaires à afficher
- * @var $link_a string[] Liste des liens à afficher
+ * @var $link_a            string[] Liste des liens à afficher
  */
+
 ?>
 
-p: {
-	"Par ",
-	em: "<?= htmlspecialchars( $News[ 'User' ] ) ?>",
-	", le <?= $News[ 'dateadd' ] ?>"
-},
-h2: "<?= htmlspecialchars( $News[ 'title' ] ) ?>",
-p: "<?= nl2br( htmlspecialchars( $News[ 'content' ] ) ) ?>",
+<p>
+	Par <em><?= htmlspecialchars( $News[ 'User' ] ) ?></em>, le <?= $News[ 'dateadd' ] ?>
+</p>
+<h2><?= htmlspecialchars( $News[ 'title' ] ) ?></h2>
+<p><?= nl2br( htmlspecialchars( $News[ 'content' ] ) ) ?></p>
 
 <?php if ( $News[ 'dateadd' ] != $News[ 'dateupdate' ] ): ?>
-	p: {
-		style: "text-align: right;"
-		small: {
-			em: "Modifiée le <?= $News[ 'dateupdate' ] ?>"
-		}
-	}
+	<p style="text-align: right;">
+		<small><em>Modifiée le <?= $News[ 'dateupdate' ] ?></em></small>
+	</p>
 <?php endif; ?>
 
-p: {
-	a: "Ajouter un commentaire" {
-		href: "<?= $link_a['putInsertComment'] ?>"
-	}
-}
+<p>
+	<a href="<?= $link_a[ 'putInsertComment' ] ?>">Ajouter un commentaire</a>
+</p>
 
 <?php if ( empty( $Comment_list_a ) ): ?>
-	p: "Aucun commentaire n'a encore été posté. Soyez le premier à en laisser un !"
+	<p>
+		Aucun commentaire n'a encore été posté. Soyez le premier à en laisser un !
+	</p>
 <?php endif; ?>
 
 <?php foreach ( $Comment_list_a as $Comment ): ?>
-	fieldset: {
-		legend: {
-			"Posté par ",
-			strong: "<?= htmlspecialchars( $Comment[ 'author' ] ) ?>",
-			"le <?= $Comment[ 'date' ] ?>",
-			<?php if (!empty($Comment['action_a'])): ?>
-				" - ",
-				<?php foreach ($Comment['action_a'] as $action_a): ?>
-					a: "<?= $action_a['label'] ?>",
-					href: "<?= $action_a['link'] ?>",
+	<fieldset>
+		<legend>
+			Posté par <strong><?= htmlspecialchars( $Comment[ 'author' ] ) ?></strong> le <?= $Comment[ 'date' ] ?>
+			<?php if ( !empty( $Comment[ 'action_a' ] ) ): ?>
+				-
+				<?php foreach ( $Comment[ 'action_a' ] as $action_a ): ?>
+					<a href=<?= $action_a[ 'link' ] ?>><?= $action_a[ 'label' ] ?></a>
 				<?php endforeach; ?>
 			<?php endif; ?>
-		}
-		p: "<?= nl2br( htmlspecialchars( $Comment[ 'content' ] ) ) ?>"
-	}
+		</legend>
+		<p>
+			<?= nl2br( htmlspecialchars( $Comment[ 'content' ] ) ) ?>
+		</p>
+	</fieldset>
 <?php endforeach; ?>
 
-p: {
-	a: "Ajouter un commentaire" {
-		href: "<?= $link_a['putInsertComment'] ?>"
-	}
-}
+<p>
+	<a href="<?= $link_a[ 'putInsertComment' ] ?>">Ajouter un commentaire</a>
+
+	
+	
+	
+	
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js"></script>
+<script type="text/javascript">
+	// Test
+	var comment = {
+		author  : 'Mon_auteur',
+		content : 'Ceci est un contenu.',
+		date    : '13/10/2016 à 14h24'
+	};
+	
+	// Transformer la variable en string JSON
+	var string = JSON.stringify( comment );
+	var html   = jQuery.parseJSON( string );
+	
+	// On a construit un objet JS à partir du JSON. Maintenant on veut afficher le nouveau commentaire
+	
+	node = document.createElement('fieldset');
+	node2 = document.createElement('legend');
+	node.appendChild(node2);
+	node2.innerHTML = 'Posté par <strong>'+html['author']+'</strong> le '+html[ 'date' ];
+	node3 = document.createElement('p');
+	node.appendChild(node3);
+	node3.textContent = html['content'];
+	//console.log(document.getElementsByTagName('fieldset'));
+	node = document.getElementById('main').insertBefore(node, document.getElementsByTagName('fieldset')[0]);
+</script>
--- App/Frontend/templates/layout.json.php
+++ App/Frontend/templates/layout.json.php
@@ -8,117 +8,58 @@
 
 require 'layout.html.php';
 ?>
-	
-<?php /*	<div id="test">
-		'blabla'
-	</div>
 
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js"></script>
-	<script type="text/javascript">
-		// Test avec le header
-		var content = {
-			div: {
-				id: "Mon super site",
-				name: "blabla",
-			}
-		};
-		
-		// Transformer la variable en string JSON
-		var string = JSON.stringify( content );
-		html       = jQuery.parseJSON( string );
-		
-		function printRecursively( object ) {
-			// Si on n'a plus un objet, on arrête la récursion
-			if ( typeof object === 'object' ) {
-				var data = [];
-				for ( var element in object ) {
-					if ( typeof object[ element ] !== 'object' ) {
-						// Si le fils n'est pas un objet, on retourne sa valeur
-						data[ element ] = object[ element ];
-					}
-					else {
-						var node = document.createElement( element );
-						// Sinon on ajoute le noeud fils
-						child    = printRecursively( object[ element ] );
-						
-						for ( var content in child ) {
-							if (content == 'value') {
-								// Ajouter le contenu entre les balises
-								node.innerHTML = child[content];
-							}
-							else if ( typeof child === 'Node' ) {
-								node.appendChild( child );
-							}
-							else {
-								node.setAttribute( content, child[ content ] );
-							}
-						}
-						return node;
-					}
-				}
-				return data;
-			}
+
+<?php
+/*
+ * 	// Test
+	var comment = {
+		comment: {
+			fk_SNC: '18'
+			author: 'Mon_auteur'
+			content: 'Ceci est un contenu.'
+			date: '13/10/2016 à 14h24'
 		}
-		
-		node = printRecursively( html );
-		document.getElementById('test').appendChild(node);
-		console.log(document.getElementById('test'));
+	};
 	
-	</script>
-<?php /*
-{
-	html: {
-		head: {
-			title: "<?= isset( $title ) ? $title : 'Mon super site' ?>",
-			meta: {
-				charset: "utf-8"
-			},
-			link: {
-				rel: "stylesheet",
-				href: "/css/Envision.css",
-				type: "text/css"
-			}
-		}
+	// Transformer la variable en string JSON
+	var string = JSON.stringify( content );
+	html       = jQuery.parseJSON( string );
 	
-		body: {
-			div: {
-				id: "wrap",
-				header: {
-					h1: {
-						a: "Mon super site" {
-							href: "<?= $layout_link_a['Frontend-buildIndex']?>"
-						}
-					},
-					p: "Comment ça, il n'y a presque rien ?"
+	function printRecursively( object ) {
+		// Si on n'a plus un objet, on arrête la récursion
+		if ( typeof object === 'object' ) {
+			var data = [];
+			for ( var element in object ) {
+				if ( typeof object[ element ] !== 'object' ) {
+					// Si le fils n'est pas un objet, on retourne sa valeur
+					data[ element ] = object[ element ];
 				}
-			
-			nav:
-				ul:
-					<?php foreach ($menu_a as $element_a): ?>
-						li: {
-							a: "<?= $element_a['label']?>" {
-								href: "<?= $element_a['link'] ?>"
-							}
+				else {
+					var node = document.createElement( element );
+					// Sinon on ajoute le noeud fils
+					child    = printRecursively( object[ element ] );
+					
+					for ( var content in child ) {
+						if (content == 'value') {
+							// Ajouter le contenu entre les balises
+							node.innerHTML = child[content];
 						}
-					<?php endforeach; ?>
-				}
-			}
-			
-			div: {
-				id: "content-wrap",
-				section: {
-					id: "main",
-					<?php if(isset($flash)): ?>
-						p: "<?= $flash ?>" {
-							style: "text-align: center;"
+						else if ( typeof child === 'Node' ) {
+							node.appendChild( child );
 						}
-					<?php endif ?>
-					<?= $content ?>
+						else {
+							node.setAttribute( content, child[ content ] );
+						}
+					}
+					return node;
 				}
 			}
-			
-			footer: {
-			}
+			return data;
 		}
 	}
-}*/
+	
+	node = printRecursively( html );
+	document.getElementById('test').appendChild(node);
+	console.log(document.getElementById('test'));
+ */
