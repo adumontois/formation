d68599bd362efb19014888a953db8f16c6e56e90
adumontois <a.dumontois@dreamcentury.com>
2016-10-14 16:35:44 +0200

    Handled errors in json form

--- App/Frontend/Modules/News/Views/buildNews.html.php
+++ App/Frontend/Modules/News/Views/buildNews.html.php
@@ -76,57 +76,68 @@ $js_data_action = $News[ 'action_a' ][ 0 ][ 'insert_comment_json' ]; ?>
 			dataType : "json", // et non datatype
 			success  : function( json, status ) {
 				// Cette fonction se déclenche dès lors que l'URL est trouvée !
-				console.log('test');
+
 				if ( json.master_code != 0 ) {
 					console.error( 'Aie aie aie je m\'arrete la car j\'ai rencontré une erreur' );
 					return;
 				}
 				
-				// Ajout des erreurs
-				console.log(json);
-				
-				
+				// Suppression des anciens messages d'erreur
+				$(".form_error").remove();
 				
 				var Comment = json.content.Comment;
 				
-				// Afficher le nouveau commentaire. On le formate en HTML.
-				var new_comment_header;
-				// Structure de base avec le header du message
-				var new_comment = $( "<fieldset class=\"js-comment\"></fieldset>" )
-						.append( new_comment_header = $( "<legend></legend>" )
-								.append( "Posté par ", $( "<strong></strong>" )
-										.text( Comment.author ), ' le ', Comment.date ) );
-				
-				// Ajout des actions au header
-				if ( 0 !== Comment.action_a.length ) {
-					new_comment_header.append( ' - ' );
-					for ( action in Comment.action_a ) {
-						new_comment_header.append( $( "<a></a>" )
-								.attr( "href", Comment.action_a[ action ].link )
-								.text( Comment.action_a[ action ].label + ' ' ) );
+				// Il y a des erreurs : on n'affiche pas le commentaire.
+				if (null !== json.content.Comment.error_a) {
+					var bad_field;
+					for (input in Comment.error_a) {
+						bad_field = ($("<div class = \"form_error\"></div>").text(Comment.error_a[input]));
+						$(bad_field).insertBefore($this.find( '[name='+input+']'));
 					}
 				}
-				
-				// Ajout du contenu
-				new_comment.append( $( "<p></p>" ).text( Comment.content ) );
-				
-				// Supprimer le message "pas de commentaires"
-				var no_comment_message = $( "#no-comment" );
-				if ( no_comment_message ) {
-					// S'il n'y a pas encore de commentaire, on retire le message disant qu'il n'y a pas de commentaire.
-					no_comment_message.remove();
-				}
-				
-				// Endroit d'insertion
-				// Prendre "au-dessus du premier commentaire"
-				insert_location = $( ".js-comment" )[ 0 ];
-				if ( typeof insert_location === 'undefined' ) {
-					// S'il n'y a pas de commentaire, on insère au dessous du premier formulaire
-					$(new_comment).insertAfter($( "#insert_comment_1" ));
-				}
 				else {
-					// Insérer le commentaire en top commentaire
-					$( new_comment ).insertBefore( insert_location );
+					// Pas d'erreur = afficher le nouveau commentaire. On le formate en HTML.
+					var new_comment_header;
+					// Structure de base avec le header du message
+					var new_comment = $( "<fieldset class=\"js-comment\"></fieldset>" )
+							.append( new_comment_header = $( "<legend></legend>" )
+									.append( "Posté par ", $( "<strong></strong>" )
+											.text( Comment.author ), ' le ', Comment.date ) );
+					
+					// Ajout des actions au header
+					if ( 0 !== Comment.action_a.length ) {
+						new_comment_header.append( ' - ' );
+						for ( action in Comment.action_a ) {
+							new_comment_header.append( $( "<a></a>" )
+									.attr( "href", Comment.action_a[ action ].link )
+									.text( Comment.action_a[ action ].label + ' ' ) );
+						}
+					}
+					
+					// Ajout du contenu
+					new_comment.append( $( "<p></p>" ).text( Comment.content ) );
+					
+					// Supprimer le message "pas de commentaires" si besoin
+					var no_comment_message = $( "#no-comment" );
+					if ( no_comment_message ) {
+						// S'il n'y a pas encore de commentaire, on retire le message disant qu'il n'y a pas de commentaire.
+						no_comment_message.remove();
+					}
+					
+					// Endroit d'insertion
+					// Prendre "au-dessus du premier commentaire"
+					insert_location = $( ".js-comment" )[ 0 ];
+					if ( typeof insert_location === 'undefined' ) {
+						// S'il n'y a pas de commentaire, on insère au dessous du premier formulaire
+						$( new_comment ).insertAfter( $( "#insert_comment_1" ) );
+					}
+					else {
+						// Insérer le commentaire en top commentaire
+						$( new_comment ).insertBefore( insert_location );
+					}
+					
+					// Vider le message entré par l'utilisateur pour éviter les doublons
+					$this.find('[name=content]').val('');
 				}
 			}
 		} );
